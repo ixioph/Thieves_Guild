f7cb9e676bf7e7bb1ba77f6f9f52d1a582692e33
ixioph <ixioph@gmail.com>
2021-11-14 23:17:15 -0800

    adding more spirit functionality

--- contracts/loot.sol
+++ contracts/loot.sol
@@ -5,4 +5,6 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract Loot is ERC20 {
     constructor() public ERC20('Thieves Guild Loot', 'LOOT') { }
+    //mint require sender owns a card
+    //fee set by vote from card holders
 }
--- contracts/spirit.sol
+++ contracts/spirit.sol
@@ -3,16 +3,18 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/ownership/Ownable.sol';
 import '@chainlink/contracts/src/v0.6/VRFConsumerBase.sol';
 
-// suits: Wands (Clubs), Cups (Hearts), Swords (Spades), Pentacles (Diamonds)
-// values: 2-10 incrementing, Paige, Knight, Queen, King, A
-contract Spirit is ERC721, VRFConsumerBase {
+
+contract Spirit is ERC721, Ownable, VRFConsumerBase {
     using Counters for Counters.Counter;
     Counters.Counter private tokenId;
-    enum Suit {Wands, Cups, Swords, Pentacles}
-    string Title;
-    //string[] ranks; // suit::value format (10 of Pentacles == 'P::10')
+    enum Suit {Wands, Cups, Swords, Pentacles, Major_Arcana}
+    enum Value {Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Page, Knight, Queen, King,
+                The_Fool, The_Magician, The_High_Priestess, The_Empress, The_Emperor, The_Heirophant, 
+                The_Lovers, The_Chariot, Strength, The_Hermit, Wheel_of_Fortune,Justice, The_Hanged_man, 
+                Death, Temperance, The_Devil, The_Tower, The_Star, The_Moon, The_Sun, Judgement, The_World}
     struct Avatar { // values editable by the token owner
         uint8 face;
         uint8 hair;
@@ -23,6 +25,9 @@ contract Spirit is ERC721, VRFConsumerBase {
     mapping (bytes32 => string) public requestIdToTokenURI;
     mapping (uint256 => Suit) public tokenIdToSuit;
     mapping (bytes32 => uint256) public requestIdToTokenId;
+    mapping (uint256 => Value) public tokenIdToCardValue;
+    mapping (address => string) public cardHolderToTitle;
+    mapping (address => Avatar) public cardHolderToAvatar;
 
     bytes32 internal keyHash;
     uint256 internal fee;
@@ -47,9 +52,28 @@ contract Spirit is ERC721, VRFConsumerBase {
         //uint256 newItemId = tokenId;
         _safeMint(cardHolder, newItemId);
         _setTokenURI(newItemId, tokenURI);
-        Suit suit = Suit(num % 4);
+        if (num % 333 != 33) {
+            Suit suit = Suit(num % 4);
+            Value value = Value(num % 14);
+        }
+        else {
+            Suit suit = Suit(4);
+            Value value = Value((num % 22) + 14);
+        }
+        
         tokenIdToSuit[newItemId] = suit;
+        tokenIdToCardValue[newItemId] = value;
         requestIdToTokenId[requestId] = newItemId;
+    }
+
+    function setTitle(string memory _title, address _target) public onlyOwner{
+        cardHolderToTitle[_target] = _title;
+    }
+
+    function setAvatar(uint8 _face, uint8 _hair, uint8 _wear) public {
+        // require that sender holds a token
+        Avatar avatar = Avatar(_face, _hair, _wear);
+        cardHolderToAvatar[msg.sender] = avatar;
     }
 
 }
